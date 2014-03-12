40b3b284b4a95c1050cb1043c40d8cc68de35a2e
Drem Darios <vidioholic@aol.com>
2014-03-11 23:23:55 -0700

    Initial commit

---
+++ photosharing/src/main/java/cs646/assignment3/photosharing/MainActivity.java
@@ -0,0 +1,49 @@
+package cs646.assignment3.photosharing;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Message;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AsyncTask<String, Integer, Message> courseTask =
+                new UserListTask().execute("");
+
+        // create list of users
+        // pass list to list activity
+
+        // Pass the login a connection.
+        Intent login = new Intent(getApplicationContext(), UserListActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("connection",connection);
+        login.putExtra("connection", connection);
+
+        startActivity(login);
+
+    }
+
+    /**
+     *
+     */
+    private class UserListTask extends AsyncTask<String, Integer, String>
+    {
+        @Override
+        protected String doInBackground(String... params)
+        {
+            // request course list
+            String message = new CourseListMessage();
+            manager.sendMessage(message);
+            Message reply = manager.getMessage();
+
+            return reply;
+        }
+
+    }
+
+}
