6a3b34a4532140fcf52ae60031412590eced39f6
Drem Darios <vidioholic@aol.com>
2014-03-12 00:11:41 -0700

    Removed volley

--- photosharing/src/main/java/cs646/assignment3/photosharing/MainActivity.java
+++ photosharing/src/main/java/cs646/assignment3/photosharing/MainActivity.java
@@ -4,16 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Message;
 
 public class MainActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        AsyncTask<String, Integer, Message> courseTask =
-                new UserListTask().execute("");
+//        setContentView(R.layout.activity_main);
+//        AsyncTask<String, Integer, Message> courseTask =
+//                new UserListTask().execute("");
 
         // create list of users
         // pass list to list activity
@@ -21,9 +20,9 @@ public class MainActivity extends Activity {
         // Pass the login a connection.
         Intent login = new Intent(getApplicationContext(), UserListActivity.class);
         Bundle bundle = new Bundle();
-        bundle.putSerializable("connection",connection);
-        login.putExtra("connection", connection);
-
+//        bundle.putSerializable("connection",connection);
+//        login.putExtra("connection", connection);
+        RequestQueue rq = Volley.newRequestQueue(this);
         startActivity(login);
 
     }
@@ -37,11 +36,12 @@ public class MainActivity extends Activity {
         protected String doInBackground(String... params)
         {
             // request course list
-            String message = new CourseListMessage();
-            manager.sendMessage(message);
-            Message reply = manager.getMessage();
-
-            return reply;
+//            String message = new CourseListMessage();
+//            manager.sendMessage(message);
+//            Message reply = manager.getMessage();
+//
+//            return reply;
+            return null;
         }
 
     }
