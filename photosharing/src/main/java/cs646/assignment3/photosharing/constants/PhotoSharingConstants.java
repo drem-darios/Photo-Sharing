685625af7c28a78a5b3e89bf314b04d8b88a58c0
Drem Darios <vidioholic@aol.com>
2014-03-16 21:37:28 -0700

    Added constants for project.

---
+++ photosharing/src/main/java/cs646/assignment3/photosharing/constants/PhotoSharingConstants.java
@@ -0,0 +1,16 @@
+package cs646.assignment3.photosharing.constants;
+
+/**
+ * Created by Drem on 3/15/14.
+ */
+public class PhotoSharingConstants {
+
+    /** URL CONSTANTS **/
+    public static final String HOST = "http://bismarck.sdsu.edu/photoserver/";
+    public static final String PHOTO_LIST_PATH = "userphotos/";
+    public static final String PHOTO_PATH = "photo/";
+    public static final String USER_LIST_PATH = "userlist/";
+    /** BUNDLE KEY CONSTANTS **/
+    public static final String USER_KEY = "user";
+    public static final String PHOTO_KEY = "photo";
+}
