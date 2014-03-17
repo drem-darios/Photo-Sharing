b4a1e0059794f3efed0c5271b29323cf838f4a44
Drem Darios <vidioholic@aol.com>
2014-03-16 21:41:20 -0700

    Added DB file and constants.

---
+++ photosharing/src/main/java/cs646/assignment3/photosharing/constants/DBConstants.java
@@ -0,0 +1,16 @@
+package cs646.assignment3.photosharing.constants;
+
+/**
+ * Created by Drem on 3/16/14.
+ */
+public class DBConstants {
+    /** DB CONSTANTS **/
+    public static final String DEFAULT_DB = "photosharing";
+    public static final String DEFAULT_USERS_TABLE = "users";
+    public static final String DEFAULT_USER_PHOTOS_TABLE = "photos";
+    public static final int DEFAULT_VERSION = 1;
+    /** DatabaseKeys **/
+    public static final String ID_KEY = "id";
+    public static final String NAME_KEY = "name";
+    public static final String USER_ID_KEY = "user_id";
+}
